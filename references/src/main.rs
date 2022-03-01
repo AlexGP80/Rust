2400035a0e5ec82eefa2c8942813a9ef4c7be491
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-01 22:34:02 +0100

    4.2. Example of only one Mutable Reference to a particular piece of data at a time

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,9 +1,19 @@
+// 4.2. Example of Mutable References
+// fn main() {
+//     let mut s = String::from("hello");
+//
+//     change(&mut s);
+// }
+//
+// fn change(some_string: &mut String) {
+//     some_string.push_str(", world");
+// }
+
+// 4.2. Example of only one Mutable Reference to a particular piece of data at a time
 fn main() {
     let mut s = String::from("hello");
+    let r1 = &mut s;
+    let r2 = &mut s;
 
-    change(&mut s);
-}
-
-fn change(some_string: &mut String) {
-    some_string.push_str(", world");
+    println!("{}, {}", r1, r2);
 }
