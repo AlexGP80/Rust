bcd0cc3d11a556c15b79fa596c4422d7669dbc4a
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-01 23:47:20 +0100

    non string slices

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -3,23 +3,43 @@ fn main() {
 
     // `first_word` works on slices of `String`s, whether partial or whole
     let word = first_word(&my_string[0..6]);
+    println!("{}", word);
     let word = first_word(&my_string[..]);
+    println!("{}", word);
     // 'first_word' also works on references to 'String's, which are equivalent
     // to whole slices of 'String's
     let word = first_word(&my_string);
+    println!("{}", word);
 
     let my_string_literal = "hello world";
+    println!("{}", word);
 
     // first_word works on slices of string literals, whether partial or whole
     let word = first_word(&my_string_literal[0..6]);
+    println!("{}", word);
     let word = first_word(&my_string_literal[..]);
+    println!("{}", word);
 
     // Because string literals *are* string slices already,
     // this works too, without the slice syntax!
     let word = first_word(my_string_literal);
 
+    println!("{}", word);
 
+    non_string_slices()
 
+}
+
+fn non_string_slices() {
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+
+    assert_eq!(slice, &[2, 3]);
+
+    for item in slice.iter() {
+        println!("{}", item)
+    }
 }
 
 fn first_word(s: &str) -> &str {
