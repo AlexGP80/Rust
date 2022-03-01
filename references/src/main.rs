de149f91e08e0ca15306a84705dd4817dc1b94c8
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-01 22:50:32 +0100

    4.2 Dangling References Compiler won't allow us to erroneously create a dangling pointer

--- references/src/main.rs
+++ references/src/main.rs
@@ -9,15 +9,27 @@
 //     some_string.push_str(", world");
 // }
 
-// 4.2. Non-Lexical Lifetimes (NLL)
+// // 4.2. Non-Lexical Lifetimes (NLL)
+// fn main() {
+//     let mut s = String::from("hello");
+//     let r1 = &s; // no problem
+//     let r2 = &s; // no problem
+//     println!("{} and {}", r1, r2);
+//     // variables r1 and r2 will not be used after this point
+//
+//     let r3 = &mut s; // no problem
+//
+//     println!("{}", r3);
+// }
+
+// 4.2 Dangling References
 fn main() {
-    let mut s = String::from("hello");
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{} and {}", r1, r2);
-    // variables r1 and r2 will not be used after this point
-
-    let r3 = &mut s; // no problem
-
-    println!("{}", r3);
+    let reference_to_nothing = dangle();
 }
+
+fn dangle() -> &String { // dangle returns a refrence to a String
+    let s = String::from("hello"); // s is a new String
+
+    &s // we return a reference to the String, s
+} // Here, s goes out of scope, and is dropped. Its memory goes away.
+  // Danger!
