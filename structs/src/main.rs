59b3c715b89e3b49acb1d30339a0db6073a74ce7
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-02 11:38:24 +0100

    Creating instances with other instances with Struct Update Syntax

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -14,6 +14,22 @@ fn main() {
     };
 
     user1.email = String::from("anotheremail@example.com");
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    println!(
+        "USER DATA\n=========\n{}\n{}\n{}\n{}\n",
+        user2.username, user2.email, user2.active, user2.sign_in_count
+    );
+
+    // Next line causes compile error because user1 username String was moved to user2
+    // println!(
+    //     "USER DATA\n=========\n{}\n{}\n{}\n{}\n",
+    //     user1.username, user1.email, user1.active, user1.sign_in_count
+    // );
 }
 
 fn build_user(email: String, username: String) -> User {
