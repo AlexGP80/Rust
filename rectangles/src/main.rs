df590dacf56a9c17e4eedcecf6586896b22e2297
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-02 12:17:45 +0100

    Added Useful Functionality with Derived Traits

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -9,10 +10,24 @@ fn main() {
         height: 50,
     };
 
+    println!("rect1 is {:#?}", rect1);
+
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
     );
+
+    debug_macro();
+}
+
+fn debug_macro() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
