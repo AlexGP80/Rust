14313cf8bf52c73f057b9ac994f9d7722874923e
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-03 15:12:05 +0100

    Starting Relative Paths with super

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,7 +1,9 @@
 #![allow(unused)]
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {
+            println!("add_to_waitlist()");
+        }
         fn seat_at_table() {}
     }
     mod serving {
@@ -9,4 +11,27 @@ mod front_of_house {
         fn server_order() {}
         fn take_payment() {}
     }
+}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {
+        println!("cook_order()");
+    }
+}
+
+fn serve_order() {
+    println!("serve_order()");
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
