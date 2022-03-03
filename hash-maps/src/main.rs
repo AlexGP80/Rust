7ecf8f6368008eb65d83fb35296d5b5ceccfb735
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-03 23:09:57 +0100

    Hash Maps. Creating a new Hash Map

---
+++ hash-maps/src/main.rs
@@ -0,0 +1,9 @@
+#![allow(unused)]
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Real Madrid"), 4);
+    scores.insert(String::from("Barça"), 1);
+}
