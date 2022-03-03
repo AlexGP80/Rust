9971bac1f1140bcff159be9ff12ac7289433dd2d
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-03 13:42:51 +0100

    Same as before but using if-let

--- matches/src/main.rs
+++ matches/src/main.rs
@@ -91,8 +91,7 @@ fn display(x: Option<String>) {
 // if let vs match
 fn if_let_vs_match() {
     let config_max = Some(3u8);
-    match config_max {
-        Some(max) => println!("The maximum is configured to be {}", max),
-        _ => (),
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
     }
 }
