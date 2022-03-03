2a9864a81411a62cd06e146f3a55acd7f7f68b4b
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-03 12:35:20 +0100

    Matches 101

---
+++ matches/src/main.rs
@@ -0,0 +1,21 @@
+#![allow(unused)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: &Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    let my_coin = Coin::Penny;
+    println!("{}", value_in_cents(&my_coin));
+}
