edfce902741f5edaf847214cfc10af260e59a1ca
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-04 00:51:57 +0100

    Exercise 8.1. Put random integers in a vector and show the mean, median and mode

--- medianmode/src/main.rs
+++ medianmode/src/main.rs
@@ -1,7 +1,8 @@
 #![allow(unused)]
 use rand::Rng;
+use std::collections::HashMap;
 
-const NUMBER_OF_NUMBERS: u8 = 10;
+const NUMBER_OF_NUMBERS: u8 = 255;
 
 fn main() {
     let mut numbers: Vec<u8> = Vec::new();
@@ -10,4 +11,34 @@ fn main() {
         numbers.push(number);
     }
     println!("{:?}", numbers);
+    numbers.sort_unstable();
+
+    let mut sum: u32 = 0;
+    for number in &numbers {
+        sum += (*number as u32);
+    }
+    let mean: f64 = (sum / (NUMBER_OF_NUMBERS as u32)).into();
+    println!("The mean is: {}", mean);
+
+    if let Some(median) = numbers.get(5) {
+        println!("The median is: {}", median);
+    }
+
+    let mut map = HashMap::new();
+    for number in &numbers {
+        let count = map.entry(number).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
+
+    let mut max_val = 0;
+    let mut max_key = 0;
+    for (k, v) in map.iter() {
+        if *v > max_val {
+            max_key = **k;
+            max_val = *v;
+        }
+    }
+    let mode = max_key;
+    println!("The mode is {}", mode);
 }
