9629b500b8e47dcda952a3e73823a7dbf1192bbd
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-03 15:43:44 +0100

    If we use pub before a struct definition, we make the struct public,  but the struct’s fields will still be private.

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -14,6 +14,22 @@ mod front_of_house {
 }
 
 mod back_of_house {
+    // If we use pub before a struct definition, we make the struct public, but the struct’s
+    // fields will still be private.
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
     fn fix_incorrect_order() {
         cook_order();
         super::serve_order();
@@ -34,4 +50,15 @@ pub fn eat_at_restaurant() {
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed to see or modify
+    // the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
 }
