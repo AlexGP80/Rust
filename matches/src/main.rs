83892181b747dba643dd9474228d4d1420f6e038
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-03 13:03:05 +0100

    Patterns that Bind to Values

--- matches/src/main.rs
+++ matches/src/main.rs
@@ -1,21 +1,35 @@
 #![allow(unused)]
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // -- snip --
+}
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: &Coin) -> u8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
 fn main() {
     let my_coin = Coin::Penny;
+    println!("{}", value_in_cents(&my_coin));
+    let my_coin = Coin::Quarter(UsState::Alabama);
     println!("{}", value_in_cents(&my_coin));
 }
