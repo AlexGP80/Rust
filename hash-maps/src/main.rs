a92ce37e74db161b9a61383c645c56b5384b1a5e
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-03 23:23:37 +0100

    Constructing a hash map using iterators and the collect method on a vector of tuples (k,v)

--- hash-maps/src/main.rs
+++ hash-maps/src/main.rs
@@ -6,4 +6,12 @@ fn main() {
 
     scores.insert(String::from("Real Madrid"), 4);
     scores.insert(String::from("Barça"), 1);
+
+    // Constructing a hash map using iterators and the collect method on a vector of tuples (k,v)
+    // Qué ponzoña
+    let teams = vec![String::from("Real Madrid"), String::from("Farsalona")];
+    let initial_scores = vec![5, 0];
+    let mut scores: HashMap<_, _> = teams.into_iter().zip(initial_scores.into_iter()).collect();
+
+    
 }
