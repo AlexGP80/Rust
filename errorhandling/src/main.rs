98bd4a58f24c246369fe60e321511a9a6f338a02
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-04 17:32:59 +0100

    Matching on Different Errors

--- errorhandling/src/main.rs
+++ errorhandling/src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     // panic!("crash and burn");
@@ -12,6 +13,14 @@ fn main() {
 
     let f = match f {
         Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
     };
 }
