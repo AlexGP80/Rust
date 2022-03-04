53aab03bcef5a2047d7d85b233a043160e4d7f74
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-04 17:36:06 +0100

    Shortcut for Panic on Error: unwrap()

--- errorhandling/src/main.rs
+++ errorhandling/src/main.rs
@@ -8,19 +8,22 @@ fn main() {
     // let v = vec![1, 2, 3];
     //
     // v[99];
+    //
+    // let f = File::open("hello.txt");
+    //
+    // let f = match f {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(fc) => fc,
+    //             Err(e) => panic!("Problem creating the file: {:?}", e),
+    //         },
+    //         other_error => {
+    //             panic!("Problem opening the file: {:?}", other_error)
+    //         }
+    //     },
+    // };
 
-    let f = File::open("hello.txt");
-
-    let f = match f {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            },
-            other_error => {
-                panic!("Problem opening the file: {:?}", other_error)
-            }
-        },
-    };
+    // Shortcuts for Panic on Error: unwrap and expect
+    let f = File::open("hello.txt").unwrap();
 }
