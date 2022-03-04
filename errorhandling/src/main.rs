b54c689c2957008a02f896ff4c0018e552546032
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-04 17:28:01 +0100

    Recoverable Errors with Result

--- errorhandling/src/main.rs
+++ errorhandling/src/main.rs
@@ -1,9 +1,17 @@
 #![allow(unused)]
+use std::fs::File;
 
 fn main() {
     // panic!("crash and burn");
 
-    let v = vec![1, 2, 3];
+    // let v = vec![1, 2, 3];
+    //
+    // v[99];
 
-    v[99];
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
 }
