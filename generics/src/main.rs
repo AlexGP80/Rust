80b782df74900a4af4bcb27c6e1a870bbc5a1d55
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-05 07:20:32 +0100

    10-10: An impl block that only applies to a struct with a particular concrete type for the generic type parameter T

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -10,8 +10,17 @@ impl<T> Point<T> {
     }
 }
 
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
 fn main() {
     let p = Point { x: 5, y: 10 };
+    let pf = Point { x: 5.0, y: 10.0 };
 
     println!("p.x = {}", p.x());
+    println!("pf.x = {}", pf.x());
+    println!("Distance from origin: {}", pf.distance_from_origin());
 }
