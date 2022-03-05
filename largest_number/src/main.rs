1b6887fe856abfc71398ca421b5f6db5effe714e
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-05 08:41:30 +0100

    largest doesn't take ownership

--- largest_number/src/main.rs
+++ largest_number/src/main.rs
@@ -14,15 +14,17 @@ fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
     let result = largest(&number_list);
+    println!("{:?}", number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
     let result = largest(&char_list);
+    println!("{:?}", char_list);
     println!("The largest char is {}", result);
 
     let str_list = vec!["puto", "rolero", "viejo", "grognard"];
     let result = largest(&str_list);
+    println!("{:?}", str_list);
     println!("The largest str is {}", result);
-
 }
