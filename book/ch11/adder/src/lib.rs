6d2869a4795699166c3e52e03d482998c02a60fd
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-06 23:00:17 +0100

    11-5: Using the Rectangle struct and its can_hold method from Chapter 5

--- book/ch11/adder/src/lib.rs
+++ book/ch11/adder/src/lib.rs
@@ -1,8 +1,19 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn exploration() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
     }
 }
