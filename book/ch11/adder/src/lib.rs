812ed91268ca874dbe540b12eb347f1d55d4b60b
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-06 23:27:25 +0100

    11-8: Testing that a condition will cause a panic!

--- book/ch11/adder/src/lib.rs
+++ book/ch11/adder/src/lib.rs
@@ -16,7 +16,21 @@ pub fn add_two(a: i32) -> i32 {
 }
 
 pub fn greeting(name: &str) -> String {
-    format!("Hello!")
+    format!("Hello {}!", name)
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
 }
 
 #[cfg(test)]
@@ -24,13 +38,19 @@ mod tests {
     use super::*;
 
     #[test]
+    #[should_panic]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
     fn greeting_contains_name() {
         let result = greeting("Carol");
-        assert!(result.contains("Carol"),
-        "Greeting did not contain name, value was `{}`",
-        result
-    );
-
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
     }
 
     #[test]
