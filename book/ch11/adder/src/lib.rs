2bed24121f492a10dce4b149b06ed20e78000acc
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-06 23:19:30 +0100

    Adding Custom Failure Messages

--- book/ch11/adder/src/lib.rs
+++ book/ch11/adder/src/lib.rs
@@ -11,9 +11,32 @@ impl Rectangle {
     }
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello!")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"),
+        "Greeting did not contain name, value was `{}`",
+        result
+    );
+
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
 
     #[test]
     fn larger_can_hold_smaller() {
