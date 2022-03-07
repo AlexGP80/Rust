3097a05992102da3c09f9514599a8d7833286969
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-07 23:10:59 +0100

    13.10: The caching logic of Cacher

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -9,6 +9,29 @@ where
     value: Option<u32>,
 }
 
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
