b48f094146d3e83042e59ff3d987fc652569c069
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-07 16:46:52 +0100

    12.21: Defining the search_case_insensitive function to lowercase the query and the line before comparing them

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -39,6 +39,19 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -64,6 +77,9 @@ safe, fast, productive.
 Pick three.
 Trust me.";
 
-        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
