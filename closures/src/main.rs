7481b4d4d96df183238f5beb9472510f9313f1f7
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-07 22:51:13 +0100

    13.5: Defining a closure and storing it in the expensive_closure variable

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -15,7 +15,11 @@ fn main() {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = simulated_expensive_calculation(intensity);
+    let expensive_closure = |num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
 
     if intensity < 25 {
         println!("Today, do {} pushups!", expensive_result);
@@ -24,10 +28,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
-            println!(
-                "Today, run for {} minutes!",
-                expensive_result
-            );
+            println!("Today, run for {} minutes!", expensive_result);
         }
     }
 }
