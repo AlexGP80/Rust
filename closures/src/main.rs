0dc7e2b16fe4c83372b0e33a4fdf4af160e700de
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-07 23:05:41 +0100

    13.9: Defining a Cacher struct that holds a closure in calculation and an optional result in value

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,6 +1,14 @@
 use std::thread;
 use std::time::Duration;
 
+struct Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
 fn simulated_expensive_calculation(intensity: u32) -> u32 {
     println!("calculating slowly...");
     thread::sleep(Duration::from_secs(2));
