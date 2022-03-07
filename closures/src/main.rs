7ea212858a8e33b01fc3b160cbfdf721b7b2058c
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-07 23:16:37 +0100

    Limpieza

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -32,15 +32,9 @@ where
     }
 }
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
-
 fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+    let simulated_user_specified_value = 15;
+    let simulated_random_number = 125;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
