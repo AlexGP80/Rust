9b19ef61c19ca0d5e0a685ccba1bb0fbbffa8a6b
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-08 12:45:09 +0100

    13.22: Testing the functionality of the next method implementation

--- shoes/src/lib.rs
+++ shoes/src/lib.rs
@@ -14,13 +14,38 @@ struct Counter {
 
 impl Counter {
     fn new() -> Counter {
-        Counter { Count: 0 }
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn calling_next_directly_on_counter() {
+        let mut counter = Counter::new();
+
+        assert_eq!(counter.next(), Some(1));
+        assert_eq!(counter.next(), Some(2));
+        assert_eq!(counter.next(), Some(3));
+        assert_eq!(counter.next(), Some(4));
+        assert_eq!(counter.next(), Some(5));
+        assert_eq!(counter.next(), None);
+    }
 
     #[test]
     fn filters_by_size() {
