43d03233dbadb37f19bf20c7a7081626d36b3b0a
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-08 11:37:58 +0100

    13.17: Calling the iterator adpator map to create a new iterator.
    This code doesn't do anything; the closure we've specified never gets 
    called. The warning reminds us why: iterator adaptors are lazy, and we 
    need to consume the iterator here.

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,5 +1,3 @@
-#![allow(unused)]
-
 fn main() {
     let v1 = vec![1, 2, 3];
 
@@ -8,4 +6,8 @@ fn main() {
     for val in v1_iter {
         println!("Got: {}", val);
     }
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    v1.iter().map(|x| x + 1);
 }
