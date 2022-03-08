5609d5478df4f59761fff5c76367f9642fef8c3d
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-08 17:17:15 +0100

    14.3: An art library with items organized into kinds and utils modules

--- art/src/lib.rs
+++ art/src/lib.rs
@@ -2,15 +2,27 @@
 //!
 //! A library for modeling artistic concepts.
 
-/// Adds one to the number given.
-///
-/// # Examples
-/// ```
-/// let arg = 5;
-/// let answer = art::add_one(arg);
-///
-/// assert_eq!(6, answer);
-/// ```
-pub fn add_one(x: i32) -> i32 {
-    x + 1
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::*;
+
+    /// Combines two primary colors in equal amounts to create a secondary color
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        SecondaryColor::Green
+    }
 }
