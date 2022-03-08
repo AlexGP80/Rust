b04d92b68ea6abdab38e94172092c1d8c385995f
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-08 15:29:39 +0100

    formatting

--- shoes/src/main.rs
+++ shoes/src/main.rs
@@ -3,12 +3,6 @@ fn main() {
     let v2 = vec![1, 2, 3, 4, 5];
 
     for (a, b) in v1.iter().zip(v2.iter().skip(1)) {
-        println!(
-            "{} * {} = {} % 3 == 0?: {}",
-            a,
-            b,
-            a * b,
-            (a * b) % 3 == 0
-        );
+        println!("{} * {} = {} % 3 == 0?: {}", a, b, a * b, (a * b) % 3 == 0);
     }
 }
