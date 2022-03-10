194f054ea1b78776e61842f1fbf4337a8e094770
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-10 12:39:34 +0100

    16.10: Sending multiple messages and pausing between each

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -19,15 +19,25 @@ fn threads() {
 fn message_passing() {
     use std::sync::mpsc;
     use std::thread;
+    use std::time::Duration;
 
     let (tx, rx) = mpsc::channel();
 
     thread::spawn(move || {
-        let val = String::from("hi");
-        tx.send(val).unwrap();
-        println!("Val is {}", val);
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
     });
 
-    let received = rx.recv().unwrap();
-    println!("Got: {}", received);
+    for received in rx {
+        println!("Got: {}", received);
+    }
 }
