127fdcc5427c742e5ca49927bf24683c1efe51fe
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-11 17:57:23 +0100

    Remove unnecessary mut's

--- ch17_3_types/src/main.rs
+++ ch17_3_types/src/main.rs
@@ -5,22 +5,22 @@ fn main() {
 
     post.add_text("I ate a salad for lunch today");
 
-    let mut post = post.request_review();
+    let post = post.request_review();
 
-    let mut post = post.reject();
-
-    let mut post = post.request_review();
-
-    post.approve();
-
-    let mut post = post.reject();
+    let post = post.reject();
 
     let mut post = post.request_review();
 
     post.approve();
+
+    let post = post.reject();
+
+    let mut post = post.request_review();
+
+    post.approve();
     post.approve();
 
-    let mut post = post.publish().into_post().expect("Not a Post");
+    let post = post.publish().into_post().expect("Not a Post");
 
     assert_eq!("I ate a salad for lunch today", post.content());
 }
