8776c00a321b3b03c3d6105bf6602d5d05268b4e
AlexGP80 <alejandrogaminopacheco@gmail.com>
2022-03-25 19:45:05 +0100

    Implementing Iterator for DoublyLinkedList I hate this XD

--- data_structures_algorithms/doubly_linked_list/src/lib.rs
+++ data_structures_algorithms/doubly_linked_list/src/lib.rs
@@ -74,6 +74,37 @@ impl Drop for DoublyLinkedList {
     }
 }
 
+// Let's implement Iterator for our doubly linked list
+pub struct ListIterator {
+    current: Link,
+}
+
+impl ListIterator {
+    fn new(start_at: Link) -> ListIterator {
+        ListIterator {
+            current: start_at,
+        }
+    }
+}
+
+impl Iterator for ListIterator {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        let current = &self.current;
+        let mut result = None;
+        self.current = match current {
+            Some(ref current) => {
+                let current = current.borrow();
+                result = Some(current.value.clone());
+                current.next.clone()
+            },
+            None => None
+        };
+        result
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
